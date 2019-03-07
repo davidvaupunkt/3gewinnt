e2f85b3bbe0b24622ebeb3a66faee81dd310de68
davidvaupunkt <48332975+davidvaupunkt@users.noreply.github.com>
2019-03-07 21:11:00 +0100

    Add files via upload

--- arena.c
+++ arena.c
@@ -18,6 +18,13 @@ char random_move(unsigned int slen){
 	return (char)(rand()%slen);
 }
 
+char isValidMove(char ** game, char move){
+	if(game[0][move] == 0){
+		return 1;
+	}
+	return 0;
+}
+
 char getLandingId(char ** game, char stack, unsigned int slen){
 	char row = 0;
 	while(row < slen-1 && game[row+1][stack] == 0){
@@ -25,46 +32,49 @@ char getLandingId(char ** game, char stack, unsigned int slen){
 	}
 	return row;
 }
-/*
-void fuzzyEvolveCounter(unsigned int *count, char **game, char coordX, char coordY, char d1, char d2 , char me) {
-    char xOffs[2][4] = {{1, 1, 0, -1}, {-1,-1, 0, 1}};
-    char yOffs[2][4] = {{0,-1,-1, -1}, { 0, 1, 1, 1}};
 
-    if(0 <= coordX + xOffs[d1][d2] && coordX + xOffs[d1][d2] <= 8    &&     0 <= coordY + yOffs[d1][d2] && coordY + yOffs[d1][d2] <= 8) {
-        if(game[coordX + xOffs[d1][d2]][coordY + yOffs[d1][d2]] == me) {
-            *count += 1;
-            fuzzyEvolveCounter(count, game, coordX + xOffs[d1][d2], coordY + yOffs[d1][d2], d1, d2, me);
-        }
-    }
+
+void fuzzyEvolveCounter(unsigned int *count, char **game, char coordX, char coordY, char d1, char d2 , char me, unsigned int slen) {
+	char xOffs[2][4] = {{1, 1, 0, -1}, {-1,-1, 0, 1}};
+	char yOffs[2][4] = {{0,-1,-1, -1}, { 0, 1, 1, 1}};
+
+	if(0 <= coordX + xOffs[d1][d2] && coordX + xOffs[d1][d2] < slen	&& 	0 <= coordY + yOffs[d1][d2] && coordY + yOffs[d1][d2] < slen) {
+		if(game[coordX + xOffs[d1][d2]][coordY + yOffs[d1][d2]] == me) {
+			*count += 1;
+			fuzzyEvolveCounter(count, game, coordX + xOffs[d1][d2], coordY + yOffs[d1][d2], d1, d2, me, slen);
+		}
+	}
 }
 
 
-char isWinningMove(char **game, char player, char move) {
-    //A winning move must be valid
-    if(!isValidMove(game, move))
-        return 0;
-        
-    //Landing Coords
-    char x = move;
-    char y = getStackHeight(game, move) + 1;
-    
-    //check if this creates a win for player
-    int counter[4] = {0,0,0,0};
-    for(int dir = 0; dir <= 3; dir++) {
-        fuzzyEvolveCounter(&counter[dir], game, x, y, 0, dir, player);
-        fuzzyEvolveCounter(&counter[dir], game, x, y, 1, dir, player);
-        if(counter[i] >= 3)
-            return 1;
-    }
-    return 0;
-}
-*/
-
-int check_for_game_end(unsigned int slen, char **sf, char player, char zug){
-	if(rand()%4 == 0){
-		return 1;
+char isWinningMove(char **game, char player, char move, unsigned int slen) {
+	//A winning move must be valid
+	if(!isValidMove(game, move))
+		return 0;
+		
+	//Landing Coords
+	char y = move;
+	char x = getLandingId(game, move, slen);
+	
+	//check if this creates a win for player
+	int counter[4] = {0,0,0,0};
+	for(int dir = 0; dir <= 3; dir++) {
+		fuzzyEvolveCounter(&counter[dir], game, x, y, 0, dir, player, slen);
+		fuzzyEvolveCounter(&counter[dir], game, x, y, 1, dir, player, slen);
+		if(counter[dir] >= 2)
+			return 1;
 	}
 	return 0;
+}
+
+
+char check_for_game_end(unsigned int slen, char **sf, char player, char zug){
+	char win = isWinningMove(sf,player,zug,slen);
+	if(win==0){
+		return 0;
+	}else{
+		return player;
+	}
 }
 
 int turn(char player, char zug, unsigned int slen, char**sf){
@@ -129,9 +139,9 @@ int main(int narg, char *argv[]){
 	char winner = 0;
 	while(reason >= 0){
 		cturn = get_turn(player,slen);
+		winner = check_for_game_end(slen,spielfeld,player,cturn);
 		reason = turn(player,cturn,slen,spielfeld);
 		print_spielfeld(slen,spielfeld);
-		winner = check_for_game_end(slen,spielfeld,player,cturn);
 		if(winner > 0){	
 			reason = winner;
 			break;
